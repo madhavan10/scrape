d8fc1afd2bd19d4bf6403f6f3a0b47b7400acaf0
Madhavan Somanathan <madhavansomanathan@gmail.com>
2022-07-23 13:12:04 +0530

    running into issues with files which make network requests to fetch data

--- testWget.py
+++ testWget.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import re
+from bs4 import BeautifulSoup
 
 '''
 startURL = "https://mahadalitvikasmission.org/VMR/DistrictWiseDetailsUploaded.do"
@@ -11,34 +13,67 @@ stream = os.popen('wget -O ' + os.path.join(rootDir, "rootPage.html" + " " + sta
 print(stream.read())
 '''
 
-rootDir = os.getcwd()
-rootURL = "https://mahadalitvikasmission.org/VMR/"
-startURL = "https://mahadalitvikasmission.org/VMR/BlockWiseDetailsUploaded.do?dt=212" 
+def fetchHtml(relativeURL, outputDir, filename, maxDepth):
+    absFilename = os.path.join(outputDir, filename)
+    print('Fetching ' + absFilename)
+    stream = os.popen('wget -w 5 -O ' + '\''+ absFilename + '\'' + " " + rootURL + relativeURL)
+    exitStatus = stream.close()
+    if exitStatus is not None:
+        sys.exit('EXIT STATUS IS NOT NONE')
 
-stream = os.popen('wget -O ' + os.path.join(rootDir, "katihar.html") + " " + startURL)
-print(stream.read())
+    
+    if maxDepth == 0:
+        return
 
-stream = os.popen('grep href= katihar.html')
-grepOutput = stream.read()
+    temp = extractLinks(absFilename)
+    links, linkTexts = temp[0], temp[1]
+    newDirname = os.path.join(outputDir, filename.split('.', 1)[0])
+    try:
+        os.mkdir(newDirname)
+    except FileExistsError:
+        pass
+    for i, link in enumerate(links):
+        fetchHtml(links[i], newDirname, linkTexts[i] + '.html', maxDepth - 1) 
+    
 
-links = re.findall('href=".*"', grepOutput)
-trimmedLinks = []
-
-for link in links:
-    trimmedLinks.append(link[len('href='):].strip('"'))
-
-def fetchHtml(relativeURL, outputDir):
-    filename = relativeURL.rsplit("=", maxsplit = 1)[-1]
-    print('Fetching ' + relativeURL)
-    stream = os.popen('wget -O ' + os.path.join(outputDir, filename) + " " + rootURL + relativeURL)
 
 def extractLinks(filename):
-    stream = os.popen('grep href= ' + filename)
-    grepOutput = stream.read()
-    links = re.findall('href=".*"', grepOutput)
-    trimmedLinks = []
-    trimmedLinks.append(link[len('href='):].strip('"'))
-    return trimmedLinks
+    with open(filename) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    linkTags = soup.find_all('a')
+    links = []
+    linkTexts = []
+    for tag in linkTags:
+        links.append(tag['href'])
+        linkTexts.append(tag.string)
+        print(tag.string)
+
+    return (links, linkTexts)
+
+rootDir = os.getcwd()
+rootURL = 'https://mahadalitvikasmission.org/VMR/'
+startURL = "BlockWiseDetailsUploaded.do?dt=212" 
+
+
+fetchHtml(startURL, rootDir, 'katihar.html', maxDepth = 3)
+
+
+'''
+
+
+for file in os.listdir():
+    split = file.rsplit('.', 1)
+    if split[-1] != 'html':
+        continue
+    temp = extractLinks(file)
+    links, linkTexts = temp[0], temp[1]
+    try:
+        os.mkdir(split[0])
+    except FileExistsError:
+        pass
+    for i, link in enumerate(links):
+        fetchHtml(links[i], split[0], linkTexts[i] + '.html')
+
 
 try:
     os.mkdir("blocks")
@@ -47,10 +82,15 @@ except FileExistsError:
 
 for link in trimmedLinks:
     fetchHtml(link, "blocks")
-    
+
+stream = os.popen('grep href= katihar.html')
+grepOutput = stream.read()
+links = re.findall('href=".*"', grepOutput)
+trimmedLinks = []
+for link in links:
+    trimmedLinks.append(link[len('href='):].strip('"'))
 
 
-'''
 katiharLink = trimmedLinks[15]
 
 stream = os.popen('wget -O ' + os.path.join(rootDir, "katiharDir.html" + " " + rootURL + katiharLink))
