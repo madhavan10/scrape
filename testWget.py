9dd062309830434fc67773b692883b65db6acb59
Madhavan Somanathan <madhavansomanathan@gmail.com>
2022-07-24 06:34:05 +0530

    modified to use selenium for pages using HTML injection via javascript

--- testWget.py
+++ testWget.py
@@ -1,41 +1,53 @@
 import os
 import sys
-import re
 from bs4 import BeautifulSoup
 
-'''
-startURL = "https://mahadalitvikasmission.org/VMR/DistrictWiseDetailsUploaded.do"
-rootURL = "https://mahadalitvikasmission.org/VMR/"
-
-rootDir = os.getcwd()
-
-stream = os.popen('wget -O ' + os.path.join(rootDir, "rootPage.html" + " " + startURL))
-print(stream.read())
-'''
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def fetchHtml(relativeURL, outputDir, filename, maxDepth):
     absFilename = os.path.join(outputDir, filename)
-    print('Fetching ' + absFilename)
-    stream = os.popen('wget -w 5 -O ' + '\''+ absFilename + '\'' + " " + rootURL + relativeURL)
-    exitStatus = stream.close()
-    if exitStatus is not None:
-        sys.exit('EXIT STATUS IS NOT NONE')
-
+    seleniumRequired = (INITIAL_MAX_DEPTH - maxDepth == 2)
+    links, linkTexts = None, None
+    if seleniumRequired:
+        print('Fetching via Selenium ' + absFilename)
+        links, linkTexts = fetchHtmlViaSelenium(relativeURL, absFilename)
+    else:
+        print('Fetching ' + absFilename)
+        stream = os.popen('wget -O ' + '\''+ absFilename + '\'' + " " + rootURL + relativeURL)
+        exitStatus = stream.close()
+        if exitStatus is not None:
+            sys.exit('EXIT STATUS IS NOT NONE')
     
     if maxDepth == 0:
         return
-
-    temp = extractLinks(absFilename)
-    links, linkTexts = temp[0], temp[1]
-    newDirname = os.path.join(outputDir, filename.split('.', 1)[0])
+    
+    newDirname = os.path.join(outputDir, filename.rsplit('.', 1)[0])
     try:
         os.mkdir(newDirname)
     except FileExistsError:
         pass
+    
+    if not seleniumRequired:
+        temp = extractLinks(absFilename)
+        links, linkTexts = temp[0], temp[1]
+    
     for i, link in enumerate(links):
         fetchHtml(links[i], newDirname, linkTexts[i] + '.html', maxDepth - 1) 
     
-
+def fetchHtmlViaSelenium(relativeURL, absFilename):
+    driver.switch_to.new_window('tab')
+    driver.get(rootURL + relativeURL)
+    linkElements = driver.find_elements(By.TAG_NAME, 'a')
+    links = []
+    linkTexts = []
+    for elt in linkElements:
+        links.append(elt.getAttribute('href'))
+        linkTexts.append(elt.text)
+    
+    driver.close()
+    return (links, linkTexts)
 
 def extractLinks(filename):
     with open(filename) as fp:
@@ -53,9 +65,13 @@ def extractLinks(filename):
 rootDir = os.getcwd()
 rootURL = 'https://mahadalitvikasmission.org/VMR/'
 startURL = "BlockWiseDetailsUploaded.do?dt=212" 
+INITIAL_MAX_DEPTH = 3
 
+# setup selenium webdriver
+chromeDriverPath = r"C:\Users\madha\chromedriver.exe"
+driver = webdriver.Chrome(executable_path = chromeDriverPath)
 
-fetchHtml(startURL, rootDir, 'katihar.html', maxDepth = 3)
+fetchHtml(startURL, rootDir, 'katihar.html', maxDepth = INITIAL_MAX_DEPTH)
 
 
 '''
